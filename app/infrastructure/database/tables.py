e90ede99209dd3a068c5f47bf13f9713ca371c7d
Arwichok <arwichok@gmail.com>
2024-06-20 08:01:38 +0300

    refactory database

--- app/infrastructure/database/tables.py
+++ app/infrastructure/database/tables.py
@@ -1,12 +1,21 @@
 from advanced_alchemy.base import orm_registry, UUIDAuditBase
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 __all__ = [
     "Todo",
+    "User",
     "orm_registry",
 ]
+
+
+class User(UUIDAuditBase):
+    name: Mapped[str]
+    todo: Mapped[list["Todo"]] = relationship(back_populates="user")
 
 
 class Todo(UUIDAuditBase):
     name: Mapped[str]
     done: Mapped[bool] = mapped_column(default=False)
+    user: Mapped["User"] = relationship(back_populates="todo")
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
